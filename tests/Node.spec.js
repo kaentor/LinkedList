3e7cb5a7fc3b46fe93e4151a6d1e25a496915c01
Kyo Euler <kyoeuler@gmail.com>
2020-06-24 13:46:12 -0500

    [feat] Node can be created with a value.

--- tests/Node.spec.js
+++ tests/Node.spec.js
@@ -6,6 +6,13 @@ describe('@kaentor/LinkedList :: Node', () => {
     const node = new Node();
 
     expect(node.data).to.be.undefined;
-    expect(node.next).to.be.undefined;
+    expect(node.next).to.be.null;
+  });
+
+  it('can create a node with value', () => {
+    const node = new Node(1);
+
+    expect(node.data).to.equal(1);
+    expect(node.next).to.be.null;
   });
 });
